10a6fdabc98cd0a7a27c093a7f573a6a809873ab
darlliu <darlliu@lavabit.com>
2013-07-31 12:51:21 -0700

    new work

--- huang/network.py
+++ huang/network.py
@@ -1,6 +1,7 @@
 import os, sys, string, numpy, math
+from copy import deepcopy
 import cPickle as pkl
-import crick.viz.js.google_chart_js as chart
+#import crick.viz.js.google_chart_js as chart
 import crick.annotate.cybert.crick_cybert_annotator as anno
 import crick.builders.protein_protein_edges.StringBuilder as sb
 from crick.utils.tabular import tabular
@@ -124,4 +125,97 @@ def routine5():
     tt.col_map(lambda x:",".join([IDS[i] for i in x.split(", ")]),"geneIds")
     open("Enrichment.csv","w").write(repr(tt))
 
-routine5()
+def routine6():
+    K=30
+    #idxs = t % "Cluster Index, K=30, data = filtered_pvals"
+    #print idxs
+
+    t= tabular()
+    t.loadf("pvalclusters.xls")
+    t.order()
+    print "O list is now" , t.order_list
+    t.col_map(int, "Cluster Index, K=30, data = filtered_pvals")
+    for ii in xrange(K):
+        print "O list is now" , t.order_list
+        ns = t.filter(lambda x: int(x)==ii,"Cluster Index, K=30, data = filtered_pvals" )
+        print t.order_list , len(ns.data), (ns % 0), len(t.data), len (t.data[0])
+        print ns % "Cluster Index, K=30, data = filtered_pvals"
+        ns= (ns % 0)
+        print "got {0:d} proteins for i={1:d}".format(len(ns),ii)
+        t.reorder()
+        try:
+            an = anno.crick_tester(species.hg19)
+            an.n.add_proteins(ns)
+            an.closed_ppi()
+            an.do_annotation_routine(f=ns+["Kmeans Cluster{0:d} of 30".format(ii)])
+            an.exportfig("Huang_K30_{0:d}.xgmml".format(ii))
+            del an
+
+            an = anno.crick_tester(species.hg19)
+            an.n.add_proteins(ns)
+            sbuilder=sb.StringProteinProteinEdgeBuilder(an.n)
+            sbuilder.build_network(closed_network=True,string_itype="binding", string_score=600);
+            an.do_annotation_routine(f=ns+["Kmeans-cluster{0:d} of 30".format(ii)])
+            an.exportfig("Huang_STRING_600_K30_{0:d}".format(ii))
+
+            del an
+            del sbuilder
+            an = anno.crick_tester(species.hg19)
+            an.n.add_proteins(ns)
+            sbuilder=sb.StringProteinProteinEdgeBuilder(an.n)
+            sbuilder.build_network(closed_network=True,string_itype="binding", string_score=900);
+            an.do_annotation_routine(f=ns+["Kmeans-cluster{0:d} of 30".format(ii)])
+            an.exportfig("Huang_STRING_900_K30_{0:d}".format(ii))
+
+            ap = ProteinAnalyzer(an.n,"./tmpanalysis")
+            ap.DAVIDAnalysis()
+            tt= tabular()
+            tt.loadf("./tmpanalysis/DAVIDReports/EnrichmentReport.xls")
+            tt.col_map(lambda x:",".join([IDS[i] for i in x.split(", ")]),"geneIds")
+        except:
+            continue
+        open("subnetworks/analysis/Huang_K30_{0:d}.csv".format(ii),"w").write(repr(tt))
+#routine6()
+def routine7():
+    t= tabular()
+    t.loadf("pvalclusters.xls")
+    t.order()
+    print "O list is now" , t.order_list
+    t.col_map(int, "Cluster Index, K=30, data = filtered_pvals")
+
+    r = tabular()
+    r.loadf("./allComplexes.csv", sep=";")
+
+    prots =  r % "subunits (UniProt IDs)"
+    CPX = {}
+    K=30
+    for i in prots:
+        for j in i.split(","):
+            j=j.strip("(").strip(")")
+            CPX[j]=prots.index(i)
+    for ii in xrange(K):
+        ns = t.filter(lambda x: int(x)==ii,"Cluster Index, K=30, data = filtered_pvals" )
+        #print t.order_list , len(ns.data), (ns % 0), len(t.data), len (t.data[0])
+        #print ns % "Cluster Index, K=30, data = filtered_pvals"
+        ns= (ns % 0)
+        print "got {0:d} proteins for i={1:d}".format(len(ns),ii)
+        rr = tabular()
+        rr.header =deepcopy (r.header)
+        rr.nc = r.nc
+        rr << ([],"Number of Subunits in cluster")
+        print rr
+        hits = {}
+        for i in ns:
+            if i in CPX:
+                print "Found a complex:", (r % 1)[CPX[i]]
+                if CPX[i] in hits:
+                    hits [CPX[i]]+=1
+                else:
+                    hits [CPX[i]]=1
+        print hits
+        for idx, num_hit in hits.items():
+            rr < (r[idx]+[num_hit])
+        rr.order(f=lambda x: int(x))
+        open("./subnetworks/complex/Cluster_{0:d}.xls".format(ii),"w").write(repr(rr))
+        t.reorder()
+routine7()
